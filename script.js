131f5ae0f225c6bd36995b8a6c57dbd4375aaf54
JaySanders95 <jaysanders1878@gmail.com>
2023-02-03 19:53:57 +0000

    fixed bug where menus would not alternate with fadeIn/fadeOut class

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-const game = ()=> {
+const game = () => {
     //player score and lives remaining
     let playerScore = 0;
     let lives = 5;
 
     //this function will remove the rules and start the game once called
-const startGame = ()=> {
-    const letsPlay = document.getElementById('lets-play')
-    const rules = document.querySelector('.rules');
-    const match = document.getElementsByClassName('match')[0];
+    const startGame = () => {
+        const letsPlay = document.getElementById('lets-play')
+        const rules = document.querySelector('.rules');
+        const match = document.querySelector('.match');
 
-console.log(letsPlay, "<=== lets play")
+    console.log(match, "this is match");
 
-    letsPlay.addEventListener('click', () => { 
-        console.log("cliocked!!!", rules)
-        rules.classList.add("fadeOut")
-      });
-}
+        letsPlay.addEventListener('click', () => {
+            rules.classList.add("fadeOut");
+            match.classList.add("fadeIn");
 
-startGame();
+            console.log(match);
+            
 
+        });
+    }
 
-
-/* WHY WONT YOU WORK?!?!? */
-
-
-
-const playMatch = () => {
-    const options = document.getElementsByClassName('buttonStyle');
-    const playerChoice = document.getElementsByClassName('players-selection');
-    const computerChoice = document.getElementsByClassName('computers-selection');
-    const computerSelections = ['rock', 'paper' , 'scissors', 'lizard', 'spock'];
-    
-   
-
-    options.forEach(option=>{
-        option.addEventListener('click', function() {
-           let randomNumber = Math.floor(Math.random() * 5);
-           let computerPicks = computerSelections[randomNumber];
-           console.log(computerPicks);
-        }
-        )
+    startGame();
 
 
-    })
-    
-};
 
-/* OR YOU?!?!?!? */
+    /* WHY WONT YOU WORK?!?!? */
 
-let = hello = "hello world";
 
-console.log(hello);
+
+
+
+    const playMatch = () => {
+        const options = document.getElementsByClassName('buttonStyle');
+        const playerChoice = document.getElementsByClassName('players-selection');
+        const computerChoice = document.getElementsByClassName('computers-selection');
+        const computerSelections = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function () {
+                let randomNumber = Math.floor(Math.random() * 5);
+                let computerPicks = computerSelections[randomNumber];
+                console.log(computerPicks);
+            })
+
+
+        })
+
+    }
+
+    playMatch();
+
 
 
 
