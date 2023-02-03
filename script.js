5b45fa541b702ba63853451b5fa4a9047de3dbfd
JaySanders95 <jaysanders1878@gmail.com>
2023-02-03 16:33:05 +0000

    started with actual game as i dont have time to waste

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+const game = ()=> {
+    //player score and lives remaining
+    let playerScore = 0;
+    let lives = 5;
+
+    //this function will remove the rules and start the game once called
 const startGame = ()=> {
-    let letsPlay = document.getElementsByClassName('rules');
-    
-    for (let i = 0; i < letsPlay.length; i++){
-    letsPlay[0].classList.add("fadeOut");
+    const letsPlay = document.getElementsByClassName('lets-play')[0]
+    const rules = document.getElementsByName('rules')[0]
+    const match = document.getElementsByClassName('match')[0]
+
+    letsPlay.addEventListener('click', () => { 
+        rules.classList.add("fadeOut")
+      });
 }
 
-    let clickBtn = document.getElementsByClassName('lets-play');
-    clickBtn.addEventListener('click', startGame());
+startGame();
+
+/* ill be back */
+
+
+
+const playMatch = () => {
+    const options = document.querySelectorAll('.selections button')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
-
-
-
